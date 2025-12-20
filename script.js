5de4411dd50709ae0c60bff8f67d4405a39d1008
t0106birth-prog <t0106birth@gmail.com>
2025-12-20 23:35:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const MY_LIFF_ID = '2006502233-yq0x2pDd';
 
     // 1. Google Apps Scriptをデプロイして発行されたURLをここに貼り付けてください。
-    const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbyJiXtD5bLa9flYYcu_NMe2VlHq9vCDsJkdjfTN7MsWYoxbdQc3AZlk88JZ7Ltv803ixA/exec'; 
+    const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbw5hAao8NV7peusTCNKJ7WwjJ-SUge7ylQrJ5b6wgQuzxkHXTIkC1wsR2M_97gHo1phlQ/exec'; 
 
     // 3. 本番通信を行う場合は false に、デモ（テスト）の場合は true にしてください。
     const USE_MOCK_BACKEND = false;
