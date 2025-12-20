e27d34a54e9c1f18c21aadcb994165b6afd752b5
t0106birth-prog <t0106birth@gmail.com>
2025-12-20 22:41:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbxL-jhXdS5SEjOSFJbPhw1h9pnAWQ0Q0XzIdjx5hXD4OgS0uAA2xvSCCNzxwmIrTRlcfA/exec'; 
 
     // 3. 本番通信を行う場合は false に、デモ（テスト）の場合は true にしてください。
-    const USE_MOCK_BACKEND = false;
+    const USE_MOCK_BACKEND = true;
     // -----------------------------------------------------------------
 
     let currentUserId = 'anonymous';
