0ec5260c70bb12e011e024afcd044e0b27df3a62
t0106birth-prog <t0106birth@gmail.com>
2025-12-31 18:27:12 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
-// -----------------------------------------------------------------
-// ★ IMPORTANT: 設定エリア
-// -----------------------------------------------------------------
-// 1. LINE Developersで発行したLIFF IDを入力してください
-const MY_LIFF_ID = 'YOUR_LIFF_ID_HERE';
+    // -----------------------------------------------------------------
+    // ★ IMPORTANT: 設定エリア
+    // -----------------------------------------------------------------
+    // 1. LINE Developersで発行したLIFF IDを入力してください
+    const MY_LIFF_ID = '2006502233-yq0x2pDd';
 
-// 2. Google Apps Scriptをデプロイして発行されたURLをここに貼り付けてください。
-const GAS_API_URL = '';
+    // 1. Google Apps Scriptをデプロイして発行されたURLをここに貼り付けてください。
+    const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbxfgpPbFITKkchDyMvmDM0ix2njAPzbwTJnEwoXuumF09YauUgRlIu8YY7ICOZHwz-hSQ/exec'; 
 
-// 3. 本番通信を行う場合は false に、デモ（テスト）の場合は true にしてください。
-const USE_MOCK_BACKEND = true;
-// -----------------------------------------------------------------
-
+    // 3. 本番通信を行う場合は false に、デモ（テスト）の場合は true にしてください。
+    const USE_MOCK_BACKEND = false;
+    // -----------------------------------------------------------------
