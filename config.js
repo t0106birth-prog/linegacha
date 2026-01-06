4107c1ae24cc0ff2b9e3bc7be8a1973fdb63f424
t0106birth-prog <t0106birth@gmail.com>
2026-01-07 00:34:07 +0900

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
     const MY_LIFF_ID = '2006502233-yq0x2pDd';
 
     // 1. Google Apps Scriptをデプロイして発行されたURLをここに貼り付けてください。
-    const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbz0NYV2k8Fwgw84tDoHHelFS0c_Dwb2NHmPtjnxmqXRZZYFIgF0EU8BEHjqoRtJ-Aa4Ig/exec'; 
+    const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbyV3xMcqBQVOK2VldKwb5UwgvV-fGxS5k7g6kxdxgDcWtSkEQfopP1fjigbQi0cJmK1oA/exec'; 
 
     // 3. 本番通信を行う場合は false に、デモ（テスト）の場合は true にしてください。
     const USE_MOCK_BACKEND = false;
